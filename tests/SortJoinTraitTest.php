f5775a37a123a5005b94e44557971c703cf1bbe6
Filip Horvat <fico7489@gmail.com>
2017-12-09 21:02:49 +0100

    remove unused

--- tests/SortJoinTraitTest.php
+++ tests/SortJoinTraitTest.php
@@ -16,23 +16,23 @@ class SortJoinTraitTest extends TestCase
         $seller = Seller::create(['title' => 1]);
         $seller2 = Seller::create(['title' => 2]);
         $seller3 = Seller::create(['title' => 3]);
-        $seller4 = Seller::create(['title' => 4]);
+        Seller::create(['title' => 4]);
 
-        $location = Location::create(['address' => 1, 'seller_id' => $seller->id]);
-        $location2 = Location::create(['address' => 2, 'seller_id' => $seller2->id]);
-        $location3 = Location::create(['address' => 3, 'seller_id' => $seller3->id]);
-        $location4 = Location::create(['address' => 3, 'seller_id' => $seller3->id]);
+        Location::create(['address' => 1, 'seller_id' => $seller->id]);
+        Location::create(['address' => 2, 'seller_id' => $seller2->id]);
+        Location::create(['address' => 3, 'seller_id' => $seller3->id]);
+        Location::create(['address' => 3, 'seller_id' => $seller3->id]);
 
-        $locationPrimary   = Location::create(['address' => 4, 'seller_id' => $seller3->id, 'is_primary' => 1]);
-        $locationSecondary = Location::create(['address' => 5, 'seller_id' => $seller3->id, 'is_secondary' => 1]);
+        Location::create(['address' => 4, 'seller_id' => $seller3->id, 'is_primary' => 1]);
+        Location::create(['address' => 5, 'seller_id' => $seller3->id, 'is_secondary' => 1]);
 
-        $order = Order::create(['number' => '1', 'seller_id' => $seller->id]);
-        $order2 = Order::create(['number' => '2', 'seller_id' => $seller2->id]);
-        $order3 = Order::create(['number' => '3', 'seller_id' => $seller3->id]);
+        Order::create(['number' => '1', 'seller_id' => $seller->id]);
+        Order::create(['number' => '2', 'seller_id' => $seller2->id]);
+        Order::create(['number' => '3', 'seller_id' => $seller3->id]);
 
-        $orderItem = OrderItem::create(['name' => '1', 'order_id' => $seller->id]);
-        $orderItem2 = OrderItem::create(['name' => '2', 'order_id' => $seller2->id]);
-        $orderItem3 = OrderItem::create(['name' => '3', 'order_id' => $seller3->id]);
+        OrderItem::create(['name' => '1', 'order_id' => $seller->id]);
+        OrderItem::create(['name' => '2', 'order_id' => $seller2->id]);
+        OrderItem::create(['name' => '3', 'order_id' => $seller3->id]);
     }
 
     private function fetchQuery()
@@ -112,18 +112,6 @@ class SortJoinTraitTest extends TestCase
         $this->checkOrder($items, [2, 3 , 1], 3);
     }
 
-    public function testOrderByJoinJoinOnlyOnce()
-    {
-        \DB::enableQueryLog();
-        $items = OrderItem::orderByJoin('order.id')
-            ->orderByJoin('order.seller.title')
-            ->orderByJoin('order.number')
-            ->groupBy('order_items.id')->get();
-
-        $query = \DB::getQueryLog()[0]['query'];
-        $this->assertEquals(2, substr_count($query, 'left join'));
-    }
-
     public function testWhereJoin()
     {
         Order::find(1)->update(['number' => 'aaaa']);
@@ -131,15 +119,11 @@ class SortJoinTraitTest extends TestCase
         Order::find(3)->update(['number' => 'cccc']);
 
         //test where does not exists
-        $items = OrderItem::orderByJoin('order.number')
-            ->whereJoin('order.number', '=', 'dddd')
-            ->get();
+        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'dddd')->get();
         $this->assertEquals(0, $items->count());
 
         //test where does exists
-        $items = OrderItem::orderByJoin('order.number')
-            ->whereJoin('order.number', '=', 'cccc')
-            ->get();
+        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'cccc')->get();
         $this->assertEquals(1, $items->count());
 
         //test where does exists, without orderByJoin
@@ -147,24 +131,15 @@ class SortJoinTraitTest extends TestCase
         $this->assertEquals(1, $items->count());
 
         //test more where does not exists
-        $items = OrderItem::orderByJoin('order.number')
-            ->whereJoin('order.number', '=', 'bbbb')
-            ->whereJoin('order.number', '=', 'cccc')
-            ->get();
+        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'bbbb')->whereJoin('order.number', '=', 'cccc')->get();
         $this->assertEquals(0, $items->count());
 
         //test more where with orWhere exists
-        $items = OrderItem::orderByJoin('order.number')
-            ->whereJoin('order.number', '=', 'bbbb')
-            ->orWhereJoin('order.number', '=', 'cccc')
-            ->get();
+        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'bbbb')->orWhereJoin('order.number', '=', 'cccc')->get();
         $this->assertEquals(2, $items->count());
 
         //test more where with orWhere does not exists
-        $items = OrderItem::orderByJoin('order.number')
-            ->whereJoin('order.number', '=', 'dddd')
-            ->orWhereJoin('order.number', '=', 'eeee')
-            ->get();
+        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'dddd')->orWhereJoin('order.number', '=', 'eeee')->get();
         $this->assertEquals(0, $items->count());
     }
 
