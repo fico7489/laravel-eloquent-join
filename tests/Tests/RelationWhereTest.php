3cbc7e473dadb94f4c824f8ede49418e0597df04
Filip Horvat <fico7489@gmail.com>
2017-12-16 14:06:26 +0100

    change tests

--- tests/Tests/RelationWhereTest.php
+++ tests/Tests/RelationWhereTest.php
@@ -3,65 +3,28 @@
 namespace Fico7489\Laravel\EloquentJoin\Tests\Tests;
 
 use Fico7489\Laravel\EloquentJoin\Tests\Models\Seller;
-use Fico7489\Laravel\EloquentJoin\Tests\Models\Order;
-use Fico7489\Laravel\EloquentJoin\Tests\Models\OrderItem;
 use Fico7489\Laravel\EloquentJoin\Tests\TestCase;
 
-class EloquentJoinTraitTest extends TestCase
+class RelationWhereTest extends TestCase
 {
-    public function testWhereJoin()
-    {
-        Order::find(1)->update(['number' => 'aaaa']);
-        Order::find(2)->update(['number' => 'bbbb']);
-        Order::find(3)->update(['number' => 'cccc']);
-
-        //test where does not exists
-        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'dddd')->get();
-        $this->assertEquals(0, $items->count());
-
-        //test where does exists
-        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'cccc')->get();
-        $this->assertEquals(1, $items->count());
-
-        //test where does exists, without orderByJoin
-        $items = OrderItem::whereJoin('order.number', '=', 'cccc')->get();
-        $this->assertEquals(1, $items->count());
-
-        //test more where does not exists
-        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'bbbb')->whereJoin('order.number', '=', 'cccc')->get();
-        $this->assertEquals(0, $items->count());
-
-        //test more where with orWhere exists
-        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'bbbb')->orWhereJoin('order.number', '=', 'cccc')->get();
-        $this->assertEquals(2, $items->count());
-
-        //test more where with orWhere does not exists
-        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'dddd')->orWhereJoin('order.number', '=', 'eeee')->get();
-        $this->assertEquals(0, $items->count());
-    }
-
     public function testWhereOnRelationWithOrderByJoin()
     {
         //location have two where  ['is_primary => 0', 'is_secondary' => 0]
-        \DB::enableQueryLog();
         $items = Seller::orderByJoin('location.id', 'desc')->get();
         $queryTest = '/select "sellers".* from "sellers" left join "locations" on "(.*)"."seller_id" = "sellers"."id" where "(.*)"."deleted_at" is null and "locations"."is_primary" = \? and "locations"."is_secondary" = \? group by "sellers"."id" order by "(.*)"."id" desc/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
 
         //locationPrimary have one where ['is_primary => 1']
-        \DB::enableQueryLog();
         $items = Seller::orderByJoin('locationPrimary.id', 'desc')->get();
         $queryTest = '/select "sellers".* from "sellers" left join "locations" on "(.*)"."seller_id" = "sellers"."id" where "(.*)"."deleted_at" is null and "locations"."is_primary" = \? group by "sellers"."id" order by "(.*)"."id" desc/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
 
         //locationPrimary have one where ['is_secondary => 1']
-        \DB::enableQueryLog();
         $items = Seller::orderByJoin('locationSecondary.id', 'desc')->get();
         $queryTest = '/select "sellers".* from "sellers" left join "locations" on "(.*)"."seller_id" = "sellers"."id" where "(.*)"."deleted_at" is null and "locations"."is_secondary" = \? group by "sellers"."id" order by "(.*)"."id" desc/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
 
         //locationPrimary have one where ['is_primary => 1'] and one orWhere ['is_secondary => 1']
-        \DB::enableQueryLog();
         $items = Seller::orderByJoin('locationPrimaryOrSecondary.id', 'desc')->get();
         $queryTest = '/select "sellers".* from "sellers" left join "locations" on "(.*)"."seller_id" = "sellers"."id" where \("(.*)"."deleted_at" is null and "locations"."is_primary" = \? or "locations"."is_secondary" = \?\) group by "sellers"."id" order by "(.*)"."id" desc/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
@@ -71,12 +34,10 @@ class EloquentJoinTraitTest extends TestCase
     {
         $seller = Seller::find(1);
 
-        \DB::enableQueryLog();
         $seller->locationPrimary;
         $queryTest = '/select \* from "locations" where "locations"."seller_id" = \? and "locations"."seller_id" is not null and "is_primary" = \? and "locations"."deleted_at" is null limit \d/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
 
-        \DB::enableQueryLog();
         $seller->locationPrimary()->where(['is_secondary' => 1])->get();
         $queryTest = '/select \* from "locations" where "locations"."seller_id" = \? and "locations"."seller_id" is not null and "is_primary" = \? and \("is_secondary" = \?\) and "locations"."deleted_at" is null/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
--- tests/Tests/WhereJoinTest.php
+++ tests/Tests/WhereJoinTest.php
@@ -2,6 +2,8 @@
 
 namespace Fico7489\Laravel\EloquentJoin\Tests\Tests;
 
+use Fico7489\Laravel\EloquentJoin\Tests\Models\Order;
+use Fico7489\Laravel\EloquentJoin\Tests\Models\OrderItem;
 use Fico7489\Laravel\EloquentJoin\Tests\Models\Seller;
 use Fico7489\Laravel\EloquentJoin\Tests\TestCase;
 
@@ -61,5 +63,36 @@ class WhereJoinTest extends TestCase
 
         $queryTest = '/select "sellers".* from "sellers" left join "locations" on "locations"."seller_id" = "sellers"."id" left join "cities" on "cities"."id" = "locations"."city_id" where "locations"."deleted_at" is null and "locations"."is_primary" = \? and "cities"."deleted_at" is null and "cities"."name" = \? group by "sellers"."id"/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
+    }
+
+    public function testWhereJoinGeneral()
+    {
+        Order::find(1)->update(['number' => 'aaaa']);
+        Order::find(2)->update(['number' => 'bbbb']);
+        Order::find(3)->update(['number' => 'cccc']);
+
+        //test where does not exists
+        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'dddd')->get();
+        $this->assertEquals(0, $items->count());
+
+        //test where does exists
+        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'cccc')->get();
+        $this->assertEquals(1, $items->count());
+
+        //test where does exists, without orderByJoin
+        $items = OrderItem::whereJoin('order.number', '=', 'cccc')->get();
+        $this->assertEquals(1, $items->count());
+
+        //test more where does not exists
+        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'bbbb')->whereJoin('order.number', '=', 'cccc')->get();
+        $this->assertEquals(0, $items->count());
+
+        //test more where with orWhere exists
+        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'bbbb')->orWhereJoin('order.number', '=', 'cccc')->get();
+        $this->assertEquals(2, $items->count());
+
+        //test more where with orWhere does not exists
+        $items = OrderItem::orderByJoin('order.number')->whereJoin('order.number', '=', 'dddd')->orWhereJoin('order.number', '=', 'eeee')->get();
+        $this->assertEquals(0, $items->count());
     }
 }
