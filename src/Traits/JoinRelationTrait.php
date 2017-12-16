552ea87dba48e4e04ba8f56cc2080530ef930221
Filip Horvat <fico7489@gmail.com>
2017-12-16 15:12:12 +0100

    remove throw exception

--- src/Traits/JoinRelationTrait.php
+++ src/Traits/JoinRelationTrait.php
@@ -14,21 +14,13 @@ trait JoinRelationTrait
     public function __call($method, $parameters)
     {
         $softDeleteOptions = ['withTrashed', 'withoutTrashed', 'onlyTrashed'];
-        $allowedOptions = array_merge(['where', 'orWhere'], $softDeleteOptions);
 
         if ($method == 'where') {
             parent::__call('setWhereForJoin', $parameters);
         } elseif ($method == 'orWhere') {
             parent::__call('setOrWhereForJoin', $parameters);
-        }
-
-
-        if (in_array($method, $softDeleteOptions)) {
+        }elseif (in_array($method, $softDeleteOptions)) {
             parent::__call('setSoftDelete', $parameters);
-        }
-
-        if (! in_array($method, $allowedOptions)) {
-            throw new \Exception('Only allowed clauses on Join relations : ' . implode(', ', $allowedOptions));
         }
 
         return parent::__call($method, $parameters);
