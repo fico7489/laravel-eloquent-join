a13d94765a2987b6acb474c5569dfb8c8e820d8c
Filip Horvat <fico7489@gmail.com>
2017-12-16 13:22:56 +0100

    better tests

--- tests/TrashedTest.php
+++ tests/TrashedTest.php
@@ -6,48 +6,58 @@ use Fico7489\Laravel\EloquentJoin\Tests\Models\OrderItem;
 
 class TrashedTest extends TestCase
 {
-    public function testSoftDeleteHasNotRelated()
+    public function testNotRelatedWithoutTrashedDefault()
     {
-        //default (withoutTrashed)
-        $items = OrderItem::orderByJoin('name')->get();
+        OrderItem::orderByJoin('name')->get();
         $queryTest = '/select \* from "order_items" where "order_items"."deleted_at" is null order by "order_items"."name" asc/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
+    }
 
-        //withoutTrashed
-        $items = OrderItem::orderByJoin('name')->withoutTrashed()->get();
+    public function testNotRelatedWithoutTrashed()
+    {
+        OrderItem::orderByJoin('name')->withoutTrashed()->get();
         $queryTest = '/select \* from "order_items" where "order_items"."deleted_at" is null order by "order_items"."name" asc/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
+    }
 
-        //onlyTrashed
-        $items = OrderItem::orderByJoin('name')->onlyTrashed()->get();
+    public function testNotRelatedOnlyTrashed()
+    {
+        OrderItem::orderByJoin('name')->onlyTrashed()->get();
         $queryTest = '/select \* from "order_items" where "order_items"."deleted_at" is not null order by "order_items"."name" asc/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
+    }
 
-        //withTrashed
-        $items = OrderItem::orderByJoin('name')->withTrashed()->get();
+    public function testNotRelatedWithTrashed()
+    {
+        OrderItem::orderByJoin('name')->withTrashed()->get();
         $queryTest = '/select \* from "order_items" order by "order_items"."name" asc/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
     }
 
-    public function testSoftDeleteHasRelated()
+    public function testRelatedWithoutTrashedDefault()
     {
-        //default (withoutTrashed)
-        $items = OrderItem::orderByJoin('order.number')->get();
+        OrderItem::orderByJoin('order.number')->get();
         $queryTest = '/select "order_items".* from "order_items" left join "orders" on "(.*)"."id" = "order_items"."order_id" where "(.*)"."deleted_at" is null and "order_items"."deleted_at" is null group by "order_items"."id/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
+    }
 
-        //withoutTrashed
-        $items = OrderItem::orderByJoin('order.number')->withoutTrashed()->get();
+    public function testRelatedWithoutTrashed()
+    {
+        OrderItem::orderByJoin('order.number')->withoutTrashed()->get();
         $queryTest = '/select "order_items".* from "order_items" left join "orders" on "(.*)"."id" = "order_items"."order_id" where "(.*)"."deleted_at" is null and "order_items"."deleted_at" is null group by "order_items"."id/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
+    }
 
-        //onlyTrashed
-        $items = OrderItem::orderByJoin('order.number')->onlyTrashed()->get();
+    public function testRelatedOnlyTrashed()
+    {
+        OrderItem::orderByJoin('order.number')->onlyTrashed()->get();
         $queryTest = '/select "order_items".* from "order_items" left join "orders" on "(.*)"."id" = "order_items"."order_id" where "(.*)"."deleted_at" is null and "order_items"."deleted_at" is not null group by "order_items"."id/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
+    }
 
-        //withTrashed
-        $items = OrderItem::orderByJoin('order.number')->withTrashed()->get();
+    public function testRelatedWithTrashed()
+    {
+        OrderItem::orderByJoin('order.number')->withTrashed()->get();
         $queryTest = '/select "order_items".* from "order_items" left join "orders" on "(.*)"."id" = "order_items"."order_id" where "(.*)"."deleted_at" is null group by "order_items"."id/';
         $this->assertRegExp($queryTest, $this->fetchQuery());
     }
