aa7156f419a262ff569abe824cb7b14026cbd9f9
Filip Horvat <fico7489@gmail.com>
2018-08-07 10:27:19 +0200

    update

--- src/Traits/EloquentJoin.php
+++ src/Traits/EloquentJoin.php
@@ -3,36 +3,10 @@
 namespace Fico7489\Laravel\EloquentJoin\Traits;
 
 use Fico7489\Laravel\EloquentJoin\EloquentJoinBuilder;
-use Illuminate\Database\Eloquent\Builder;
 
 trait EloquentJoin
 {
     use ExtendRelationsTrait;
-
-
-    //set invalid clauses on join relations
-    public function scopeSetInvalidJoin(Builder $builder, $method, $parameters = [])
-    {
-        $this->relationNotAllowedClauses[$method] = $method;
-    }
-
-    //set where clause for join relations
-    public function scopeSetWhereForJoin(Builder $builder, $column, $operator = null, $value = null, $boolean = 'and')
-    {
-        $this->relationWhereClauses[] = ['column' => $column, 'operator' => $operator, 'value' => $value, 'boolean' => $boolean];
-    }
-
-    //set orWhere clause for join relations
-    public function scopeSetOrWhereForJoin(Builder $builder, $column, $operator = null, $value)
-    {
-        $this->relationWhereClauses[] = ['column' => $column, 'operator' => $operator, 'value' => $value, 'boolean' => 'or'];
-    }
-
-    //set soft delete clauses for join relations
-    public function scopeSetSoftDelete(Builder $builder, $param)
-    {
-        $this->softDelete = $param;
-    }
 
     public function newEloquentBuilder($query)
     {
