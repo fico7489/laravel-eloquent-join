36c02d69401530098610f3f5309d78cd737adf55
Filip Horvat <fico7489@gmail.com>
2018-10-30 13:52:36 +0100

    update

---
+++ tests/Tests/Relations/HasManyTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Fico7489\Laravel\EloquentJoin\Tests\Tests\Relations;
+
+use Fico7489\Laravel\EloquentJoin\Tests\Models\Seller;
+use Fico7489\Laravel\EloquentJoin\Tests\TestCase;
+
+class HasManyTest extends TestCase
+{
+    public function testHasMany()
+    {
+        Seller::relationJoin('locations')->get();
+
+        $queryTest = 'select "sellers".* 
+            from "sellers" 
+            left join "locations" on "locations"."seller_id" = "sellers"."id" 
+            and "locations"."deleted_at" is null 
+            group by "sellers"."id"';
+
+        $this->assertQueryMatches($queryTest, $this->fetchQuery());
+    }
+
+    public function testHasManyHasOne()
+    {
+        Seller::relationJoin('locations.city')->get();
+
+        $queryTest = 'select "sellers".* 
+            from "sellers" left join "locations" on "locations"."seller_id" = "sellers"."id" 
+            and "locations"."deleted_at" is null 
+            left join "cities" on "cities"."id" = "locations"."city_id" 
+            and "cities"."deleted_at" is null 
+            group by "sellers"."id"';
+
+        $this->assertQueryMatches($queryTest, $this->fetchQuery());
+    }
+
+    public function testHasManyBelongsTo()
+    {
+        Seller::relationJoin('locations.integrations')->get();
+
+        $queryTest = 'select "sellers".* 
+            from "sellers" 
+            left join "locations" on "locations"."seller_id" = "sellers"."id" 
+            and "locations"."deleted_at" is null 
+            left join "integrations" on "integrations"."location_id" = "locations"."id"
+            and "integrations"."deleted_at" is null 
+            group by "sellers"."id"';
+
+        $this->assertQueryMatches($queryTest, $this->fetchQuery());
+    }
+}
