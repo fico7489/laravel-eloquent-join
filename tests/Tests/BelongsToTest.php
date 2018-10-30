d6635e36cd5ace7c36a81c622314a8d900cb8080
Filip Horvat <fico7489@gmail.com>
2018-10-30 08:13:30 +0100

    update

--- tests/Tests/BelongsToTest.php
+++ tests/Tests/BelongsToTest.php
@@ -44,19 +44,29 @@ class BelongsToTest extends TestCase
             and "locations"."deleted_at" is null 
             where "orders"."deleted_at" is null 
             group by "orders"."id"';
-        
+
         $this->assertQueryMatches($queryTest, $this->fetchQuery());
     }
 
     public function testBelongsToHasOneHasMany()
     {
+        Order::relationJoin('seller.locationPrimary.locationAddressPrimary')->get();
 
-        print_r($this->fetchQuery());
-        exit;
+        $queryTest = 'select "orders".* from "orders" 
+            left join "sellers" on "sellers"."id" = "orders"."seller_id" 
+            left join "locations" on "locations"."seller_id" = "sellers"."id" 
+            and "locations"."is_primary" = ? 
+            and "locations"."deleted_at" is null 
+            left join "location_addresses" on "location_addresses"."location_id" = "locations"."id" 
+            and "location_addresses"."is_primary" = ? 
+            and "location_addresses"."deleted_at" is null 
+            where "orders"."deleted_at" is null 
+            group by "orders"."id"';
+
+        $this->assertQueryMatches($queryTest, $this->fetchQuery());
     }
 
     public function testBelongsToHasManyHasOne()
     {
-        //
     }
 }
