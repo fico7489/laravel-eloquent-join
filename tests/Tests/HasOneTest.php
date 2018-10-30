044cd094eae7c3d5bced9496878c06817bc80527
Filip Horvat <fico7489@gmail.com>
2018-10-30 13:47:42 +0100

    add more

---
+++ tests/Tests/HasOneTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Fico7489\Laravel\EloquentJoin\Tests\Tests;
+
+use Fico7489\Laravel\EloquentJoin\Tests\Models\Seller;
+use Fico7489\Laravel\EloquentJoin\Tests\TestCase;
+
+class HasOneTest extends TestCase
+{
+    public function testHasOne()
+    {
+        Seller::relationJoin('location')->get();
+
+        $queryTest = 'select "sellers".* 
+            from "sellers" 
+            left join "locations" on "locations"."seller_id" = "sellers"."id" 
+            and "locations"."is_primary" = ? 
+            and "locations"."is_secondary" = ? 
+            and "locations"."deleted_at" is null 
+            group by "sellers"."id"';
+
+        $this->assertQueryMatches($queryTest, $this->fetchQuery());
+    }
+
+    public function testHasOneBelongsTo()
+    {
+        Seller::relationJoin('location.city')->get();
+
+        $queryTest = 'select "sellers".* 
+            from "sellers" left join "locations" on "locations"."seller_id" = "sellers"."id" 
+            and "locations"."is_primary" = ?
+            and "locations"."is_secondary" = ? 
+            and "locations"."deleted_at" is null 
+            left join "cities" on "cities"."id" = "locations"."city_id" 
+            and "cities"."deleted_at" is null 
+            group by "sellers"."id"';
+
+        $this->assertQueryMatches($queryTest, $this->fetchQuery());
+    }
+
+    public function testHasOneHasMany()
+    {
+        Seller::relationJoin('location.integrations')->get();
+
+        $queryTest = 'select "sellers".* 
+            from "sellers" 
+            left join "locations" on "locations"."seller_id" = "sellers"."id" 
+            and "locations"."is_primary" = ?
+            and "locations"."is_secondary" = ? 
+            and "locations"."deleted_at" is null 
+            left join "integrations" on "integrations"."location_id" = "locations"."id"
+            and "integrations"."deleted_at" is null 
+            group by "sellers"."id"';
+
+        $this->assertQueryMatches($queryTest, $this->fetchQuery());
+    }
+}
