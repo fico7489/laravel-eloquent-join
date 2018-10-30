cbf679c42f9a874d1c4c02ccfbb2261e147d52e3
Filip Horvat <fico7489@gmail.com>
2018-10-30 13:48:50 +0100

    update

--- tests/Tests/Relations/HasOneTest.php
+++ tests/Tests/Relations/HasOneTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Fico7489\Laravel\EloquentJoin\Tests\Tests;
+namespace Fico7489\Laravel\EloquentJoin\Tests\Tests\Relations;
 
 use Fico7489\Laravel\EloquentJoin\Tests\Models\Seller;
 use Fico7489\Laravel\EloquentJoin\Tests\TestCase;
