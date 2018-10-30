ced158369422476bab044005567a44a91802a1a3
Filip Horvat <fico7489@gmail.com>
2018-10-30 15:01:41 +0100

    progress

--- tests/Tests/AggregateJoinTest.php
+++ tests/Tests/AggregateJoinTest.php
@@ -8,19 +8,50 @@ use Fico7489\Laravel\EloquentJoin\Tests\TestCase;
 
 class AggregateJoinTest extends TestCase
 {
-    public function testAvg()
-    {
-        Order::relationJoin('seller')
-            ->orderByJoin('seller.id', 'asc', EloquentJoinBuilder::AGGREGATE_AVG)
-            ->get();
-
-        $queryTest = 'select "orders".*, AVG(sellers.id) as sort 
+    private $queryTest = 'select "orders".*, SUM(sellers.id) as sort 
             from "orders" 
             left join "sellers" on "sellers"."id" = "orders"."seller_id" 
             where "orders"."deleted_at" is null 
             group by "orders"."id" 
             order by "sellers"."id" asc';
 
+    public function testAvg()
+    {
+        Order::relationJoin('seller')
+            ->orderByJoin('seller.id', 'asc', EloquentJoinBuilder::AGGREGATE_SUM)
+            ->get();
+
+        $queryTest = str_replace(EloquentJoinBuilder::AGGREGATE_SUM, EloquentJoinBuilder::AGGREGATE_SUM, $this->queryTest);
+        $this->assertQueryMatches($queryTest, $this->fetchQuery());
+    }
+
+    public function testSum()
+    {
+        Order::relationJoin('seller')
+            ->orderByJoin('seller.id', 'asc', EloquentJoinBuilder::AGGREGATE_AVG)
+            ->get();
+
+        $queryTest = str_replace(EloquentJoinBuilder::AGGREGATE_SUM, EloquentJoinBuilder::AGGREGATE_AVG, $this->queryTest);
+        $this->assertQueryMatches($queryTest, $this->fetchQuery());
+    }
+
+    public function testMax()
+    {
+        Order::relationJoin('seller')
+            ->orderByJoin('seller.id', 'asc', EloquentJoinBuilder::AGGREGATE_MAX)
+            ->get();
+
+        $queryTest = str_replace(EloquentJoinBuilder::AGGREGATE_SUM, EloquentJoinBuilder::AGGREGATE_MAX, $this->queryTest);
+        $this->assertQueryMatches($queryTest, $this->fetchQuery());
+    }
+
+    public function testMin()
+    {
+        Order::relationJoin('seller')
+            ->orderByJoin('seller.id', 'asc', EloquentJoinBuilder::AGGREGATE_MIN)
+            ->get();
+
+        $queryTest = str_replace(EloquentJoinBuilder::AGGREGATE_SUM, EloquentJoinBuilder::AGGREGATE_MIN, $this->queryTest);
         $this->assertQueryMatches($queryTest, $this->fetchQuery());
     }
 }
