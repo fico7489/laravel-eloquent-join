2a14e02e18da8f944b131f7c9b44afde075b412a
Filip Horvat <fico7489@gmail.com>
2018-10-30 14:28:05 +0100

    add OrWhereTest

--- tests/Tests/Clauses/OrWhereTest.php
+++ tests/Tests/Clauses/OrWhereTest.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: fico7489
- * Date: 30.10.2018.
- * Time: 14:25
- */
+
+namespace Fico7489\Laravel\EloquentJoin\Tests\Tests\Clauses;
+
+use Fico7489\Laravel\EloquentJoin\Tests\Models\Order;
+use Fico7489\Laravel\EloquentJoin\Tests\TestCase;
+
+class OrWhereTest extends TestCase
+{
+    public function testWhere()
+    {
+        Order::relationJoin('seller')
+            ->whereJoin('seller.id', '=', 1)
+            ->orWhereJoin('seller.id', '=', 2)
+            ->get();
+
+        $queryTest = 'select "orders".* from "orders" 
+            left join "sellers" on "sellers"."id" = "orders"."seller_id" 
+            where ("sellers"."id" = ? or "sellers"."id" = ?) 
+            and "orders"."deleted_at" is null 
+            group by "orders"."id"';
+
+        $this->assertQueryMatches($queryTest, $this->fetchQuery());
+    }
+}
