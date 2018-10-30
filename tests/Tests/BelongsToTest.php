5430c1ceda9c983823ae22057ff765b07da70f54
Filip Horvat <fico7489@gmail.com>
2018-10-30 07:43:43 +0100

    adda

---
+++ tests/Tests/BelongsToTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Fico7489\Laravel\EloquentJoin\Tests\Tests;
+
+use Fico7489\Laravel\EloquentJoin\Tests\TestCase;
+
+class BelongsToTest extends TestCase
+{
+    public function testBelongsTo()
+    {
+        //
+    }
+
+    public function testBelongsToHasOne()
+    {
+        //
+    }
+
+    public function testBelongsToHasMany()
+    {
+        //
+    }
+
+    public function testBelongsToHasOneHasMany()
+    {
+        //
+    }
+
+    public function testBelongsToHasManyHasOne()
+    {
+        //
+    }
+}
