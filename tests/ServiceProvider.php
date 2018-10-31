d3408d940ccd1c82b5c3198ae4797d8a6b5944c9
Filip Horvat <fico7489@gmail.com>
2018-10-31 13:43:41 +0100

    fix seeder

--- tests/ServiceProvider.php
+++ tests/ServiceProvider.php
@@ -17,7 +17,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     protected function loadMigrationsFrom($path)
     {
         \Artisan::call('migrate', ['--database' => 'testbench']);
-        //        \Artisan::call('migrate', ['--database' => 'mysql']);
+        //\Artisan::call('migrate', ['--database' => 'mysql']);
 
         $migrator = $this->app->make('migrator');
         $migrator->run($path);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,7 +16,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         $seller = Seller::create(['title' => 1]);
         $seller2 = Seller::create(['title' => 2]);
         $seller3 = Seller::create(['title' => 3]);
-        Seller::create(['title' => 4]);
+        $seller4 = Seller::create(['title' => 4]);
 
         Location::create(['address' => 1, 'seller_id' => $seller->id]);
         Location::create(['address' => 2, 'seller_id' => $seller2->id]);
@@ -26,13 +26,13 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         Location::create(['address' => 4, 'seller_id' => $seller3->id, 'is_primary' => 1]);
         Location::create(['address' => 5, 'seller_id' => $seller3->id, 'is_secondary' => 1]);
 
-        Order::create(['number' => '1', 'seller_id' => $seller->id]);
-        Order::create(['number' => '2', 'seller_id' => $seller2->id]);
-        Order::create(['number' => '3', 'seller_id' => $seller3->id]);
+        $order = Order::create(['number' => '1', 'seller_id' => $seller->id]);
+        $order2 = Order::create(['number' => '2', 'seller_id' => $seller2->id]);
+        $order3 = Order::create(['number' => '3', 'seller_id' => $seller3->id]);
 
-        OrderItem::create(['name' => '1', 'order_id' => $seller->id]);
-        OrderItem::create(['name' => '2', 'order_id' => $seller2->id]);
-        OrderItem::create(['name' => '3', 'order_id' => $seller3->id]);
+        OrderItem::create(['name' => '1', 'order_id' => $order->id]);
+        OrderItem::create(['name' => '2', 'order_id' => $order2->id]);
+        OrderItem::create(['name' => '3', 'order_id' => $order3->id]);
 
         $this->startListening();
     }
