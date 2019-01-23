94970902c2920c91859fb4750018f42122951a31
coneggo_d <dylan.coneggo@collective-thinking.com>
2019-01-23 19:01:00 +0100

    fix multiple orderByJoin + tests

--- tests/Tests/Clauses/OrderByTest.php
+++ tests/Tests/Clauses/OrderByTest.php
@@ -7,18 +7,35 @@ use Fico7489\Laravel\EloquentJoin\Tests\TestCase;
 
 class OrderByTest extends TestCase
 {
-    public function testWhere()
+    public function testOrderBy()
     {
         Order::joinRelations('seller')
             ->orderByJoin('seller.id', 'asc')
             ->get();
 
-        $queryTest = 'select orders.*, MAX(sellers.id) as sort
+        $queryTest = 'select orders.*, MAX(sellers.id) as sort0
             from "orders" 
             left join "sellers" on "sellers"."id" = "orders"."seller_id" 
             where "orders"."deleted_at" is null 
             group by "orders"."id"
-            order by sort asc';
+            order by sort0 asc';
+
+        $this->assertQueryMatches($queryTest, $this->fetchQuery());
+    }
+
+    public function testMultipleOrderBy()
+    {
+        Order::joinRelations('seller')
+            ->orderByJoin('seller.id', 'asc')
+            ->orderByJoin('seller.title', 'desc')
+            ->get();
+
+        $queryTest = 'select orders.*, MAX(sellers.id) as sort0, MAX(sellers.title) as sort1
+            from "orders" 
+            left join "sellers" on "sellers"."id" = "orders"."seller_id" 
+            where "orders"."deleted_at" is null 
+            group by "orders"."id"
+            order by sort0 asc, sort1 desc';
 
         $this->assertQueryMatches($queryTest, $this->fetchQuery());
     }
